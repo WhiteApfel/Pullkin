24ee7a23c0cb20696c03e6db03b803592f46752b
Eugene Mayer <white@pfel.ru>
2021-09-28 20:59:10 +0300

    Add: dict -> class

---
+++ pullkin/proto/notification.py
@@ -0,0 +1,35 @@
+from copy import deepcopy
+from dataclasses import dataclass
+from typing import Optional
+
+
+class NotificationData:
+    def __init__(self, data: dict = None):
+        if not data:
+            data = {}
+        self.raw_data: dict = deepcopy(data)
+        self.title: str = data.pop('title', None)
+        self.body: str = data.pop('body', None)
+        if data:
+            for k, v in data.items():
+                self.__setattr__(k, v)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class Notification:
+    def __init__(self, data: dict = None):
+        if not data:
+            data = {}
+        self.raw_data: dict = deepcopy(data)
+        self.sender_id: Optional[str] = data.pop('from', None)
+        self.priority: Optional[str] = data.pop('priority', None)
+        self.notification: NotificationData = NotificationData(data.pop('notification', None))
+        self.fcmMessageId: Optional[str] = data.pop('fcmMessageId', None)
+        if data:
+            for k, v in data.items():
+                self.__setattr__(k, v)
+
+    def __str__(self):
+        return str(self.__dict__)
