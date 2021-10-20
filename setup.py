5eb7f03630b984908d585535e5d0bc520ce21bc3
Eugene Mayer <white@pfel.ru>
2021-10-20 03:53:39 +0300

    🩹 Restore 3.6 classifier

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import find_packages, setup
 
 pullkin_classifiers = [
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
